bf7a0b67ed7ee62ebcf105a404d39ba6acc4c93d
Kumaraguru <19eucs071@skcet.ac.in>
2022-01-24 00:53:15 +0530

    todos and contributor screens

--- src/dashboard/Contributors.js
+++ src/dashboard/Contributors.js
@@ -1,5 +1,56 @@
 import React, { useState } from "react";
 
 export default function Contributors() {
-  return <div className="flex flex-col">Hello</div>;
+  return (
+    <div className="flex flex-col">
+      <div class="flex flex-wrap -mx-3 overflow-hidden">
+        <div class="my-3 px-3 w-full overflow-hidden lg:w-1/2">
+          <div class="p-1 shadow-xl  rounded-2xl">
+            <a
+              class="block p-6 white-glassmorphism    border sm:p-8 rounded-xl"
+              href=""
+            >
+              <div class=" sm:pr-8">
+                <div className="flex space-x-10">
+                  <img src="" className="h-25 w-24" />
+                  <div>
+                    <p class="mt-2 text-sm desc text-gray-300">Name</p>
+                    <p class="mt-2 text-sm desc text-gray-300">Place</p>
+
+                    <p class="mt-2 text-sm desc text-gray-300">Tags</p>
+                    <p class="mt-2 text-sm desc text-gray-300">
+                      Reputation points
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>{" "}
+        </div>
+        <div class="my-3 px-3 w-full overflow-hidden lg:w-1/2">
+          <div class="p-1 shadow-xl  rounded-2xl">
+            <a
+              class="block p-6 white-glassmorphism    border sm:p-8 rounded-xl"
+              href=""
+            >
+              <div class=" sm:pr-8">
+                <div className="flex space-x-10">
+                  <img src="" className="h-25 w-24" />
+                  <div>
+                    <p class="mt-2 text-sm desc text-gray-300">Name</p>
+                    <p class="mt-2 text-sm desc text-gray-300">Place</p>
+
+                    <p class="mt-2 text-sm desc text-gray-300">Tags</p>
+                    <p class="mt-2 text-sm desc text-gray-300">
+                      Reputation points
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/dashboard/Todos.js
+++ src/dashboard/Todos.js
@@ -1,99 +1,24 @@
 import React, { useState, Fragment } from "react";
 
-import { Menu, Transition } from "@headlessui/react";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Todos() {
   return (
     <div className="flex flex-col">
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-            Options
-          </Menu.Button>
-        </div>
-
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                    Account settings
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                    Support
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm"
-                    )}
-                  >
-                    License
-                  </a>
-                )}
-              </Menu.Item>
-              <form method="POST" action="#">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      type="submit"
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block w-full text-left px-4 py-2 text-sm"
-                      )}
-                    >
-                      Sign out
-                    </button>
-                  )}
-                </Menu.Item>
-              </form>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
       <div class="p-1 shadow-xl  rounded-2xl">
         <a
           class="block p-6 white-glassmorphism    border sm:p-8 rounded-xl"
           href=""
         >
-          <div class=" sm:pr-8">
-            <p class="mt-2 text-sm desc text-gray-300">
-              Question <span className="text-orange-300">answer</span>
-            </p>
+          <div class=" sm:pr-8 flex-col">
+            <p class="mt-2 text-sm desc text-orange-300">Question</p>
+          </div>
+          <hr className="my-4" />
+          <div className="justify-between flex">
+            <div class=" sm:pr-8 flex-col">
+              <p class="mt-2 text-sm desc text-gray-300">by Author</p>
+            </div>
+            <div class=" sm:pr-8 flex-col">
+              <p class="mt-2 text-sm desc text-gray-300">Created on</p>
+            </div>
           </div>
         </a>
       </div>
