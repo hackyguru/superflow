b2a5d280c5228252337314697fd2587cb8e3aac0
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-01-24 00:54:30 +0530

    Add tag helpers to storage utility file

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -14,9 +14,23 @@ function clear() {
   return localStorage.clear();
 }
 
+function getTags() {
+  return get("favourites") == null || get("favourites") == ""
+    ? []
+    : get("favourites").split(",");
+}
+
+function getLastTag() {
+  return get("lastTag") == null || get("lastTag") == ""
+    ? null
+    : get("lastTag");
+}
+
 module.exports = {
   save,
   remove,
   get,
-  clear
+  clear,
+  getTags,
+  getLastTag,
 };
