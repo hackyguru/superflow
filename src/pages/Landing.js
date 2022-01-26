c10ab747e9ad078d77652dd9a17b0470ad742923
Kumaraguru <19eucs071@skcet.ac.in>
2022-01-27 00:06:50 +0530

    adding landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,9 +5,29 @@ import Header from "../components/Header";
 export default function Landing() {
   return (
     <div className="gradient-bg-welcome w-full h-screen">
-      <h1 className="md:text-9xl text-5xl  pt-60 text-center heading text-white">
+      <div className="flex justify-center pt-20 items-center">
+        <img
+          src="assets/superflow3.png"
+          className="text-center w-1/3 md:w-2/12"
+        />
+      </div>
+      <h1 className="md:text-8xl text-5xl mt-4 text-center heading  text-white">
         SUPERFLOW
       </h1>
+      <h1 className="md:text-xl text-xs mt-4 text-center desc  text-white">
+        Stack Overflow tag manager and analytics dashboard
+      </h1>
+      <div className="flex justify-center">
+        <a
+          href="/dashboard"
+          className="bg-gradient-to-r from-orange-700 via-orange-500 to-yellow-500 rounded-lg p-3 mt-10 heading text-[#181B23]"
+        >
+          Launch
+        </a>
+        <a className="ml-5 bg-gray-300 rounded-lg p-3 mt-10 heading text-[#181B23]">
+          Documentation
+        </a>
+      </div>
     </div>
   );
 }
