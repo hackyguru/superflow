88052cf0973114ffcb8cce92b0d9ab3c6b913a2b
Kumaraguru <19eucs071@skcet.ac.in>
2022-01-27 01:51:55 +0530

    adding icon for tags

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -295,9 +295,23 @@ export default function Dashboard() {
           </div>
           {tags.length ? (
             <div
-              className="items-center px-4 -mx-2  h-14   text-right 
-            py-3 rounded-xl hidden md:flex"
+              className="items-center px-3  h-14   text-right 
+            white-glassmorphism py-3 rounded-xl hidden md:flex"
             >
+              <svg
+                class="w-6 h-6 text-gray-300"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
+                ></path>
+              </svg>
               {/* <Selection /> */}
               <select
                 className="ml-5 p-2 heading text-white rounded-lg glass border border-gray-300 shadow-lg"
